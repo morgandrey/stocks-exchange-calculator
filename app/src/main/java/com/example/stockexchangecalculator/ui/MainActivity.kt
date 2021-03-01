de369d43411362a19aa2b837d609eb394832d710
Andrey <morgunof-andrey@yandex.ru>
2021-03-01 23:58:46 +0300

    delete unused code

--- app/src/main/java/com/example/stockexchangecalculator/ui/MainActivity.kt
+++ app/src/main/java/com/example/stockexchangecalculator/ui/MainActivity.kt
@@ -5,11 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.stockexchangecalculator.R
 import com.example.stockexchangecalculator.databinding.ActivityMainBinding
-import com.example.stockexchangecalculator.utils.Constants
 
 class MainActivity : AppCompatActivity() {
-
-    private var backPressed: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,29 +16,4 @@ class MainActivity : AppCompatActivity() {
             R.layout.activity_main
         )
     }
-
-    /*override fun onBackPressed() {
-        if (backPressed + TIME_INTERVAL > System.currentTimeMillis()) {
-            super.onBackPressed()
-            return
-        } else {
-            Toast.makeText(
-                applicationContext,
-                "Tap back button in order to exit",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
-        backPressed = System.currentTimeMillis()
-    }
-    override fun onBackPressed() {
-        val count = supportFragmentManager.backStackEntryCount
-
-        if (count == 2 || count == 0) {
-            super.onBackPressed()
-            return
-        } else {
-            supportFragmentManager.popBackStack()
-        }
-    }*/
 }
--- app/src/main/java/com/example/stockexchangecalculator/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.stockexchangecalculator.utils
-
-object Constants {
-    const val TIME_INTERVAL = 2000
-}
